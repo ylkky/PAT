51c51dc9f7518943990c63753c5e01c482f93638
ylkky <867326302@qq.com>
2017-08-23 23:28:35 +0800

    NOTICE BIG NUM

--- 1021.cpp
+++ 1021.cpp
@@ -4,9 +4,10 @@
 #include<list>
 using namespace std;
 
-int N, **m;
+int N;
 int maxd = 0;
 vector<int> res;
+list<int>* map;
 
 int findDepth(int root);
 int findCom();
@@ -14,32 +15,30 @@ int dfs(int p, int child);
 
 int main() {
 	cin >> N;
-	m = new int*[N];
+	map = new list<int>[N];
 	int i, j, k, d;
 	for (i = 0; i < N; i++) {
-		m[i] = new int[N];
-		for (j = 0; j < N; j++)
-			m[i][j] = 0;
+		map[i].clear();
 	}
 	for (i = 1; i < N; i++) {
 		cin >> j >> k;
-		m[j-1][k-1] = m[k-1][j-1] = 1;
+		map[j - 1].push_back(k - 1);
+		map[k - 1].push_back(j - 1);
 	}
 	if ( (k = findCom()) > 1) {
-		cout << "Error: " << k << " components";
+		cout << "Error: " << k << " components" << endl;
+		return 0;
+	}
+
+	if (N == 1) {
+		cout << 1 << endl;
 		return 0;
 	}
 	for (i = 0; i < N; i++) {
 		k = 0;
-		for (j = 0; j < N; j++) 
-			k += m[i][j];
-
+		k = map[i].size();
 		if (k > 1)
 			continue;
-		//else if (k == 0) {
-		//	findCom();
-		//	return 0;
-		//}
 		else if (k == 1) {
 			d = findDepth(i);
 			if (d > maxd) {
@@ -61,17 +60,16 @@ int main() {
 
 int findDepth(int root) {
 	int i;
-	for (i = 0; i < N; i++)
-		if (m[root][i] == 1)
-			break;
-	return dfs(root, i);
+	i = map[root].front();
+	return dfs(root, i) + 1;
 }
 
 int dfs(int p, int child) {
-	int i, d = 0, max = 0;
-	for (i = 0; i < N; i++) {
-		if (m[child][i] == 1 && i != p) {
-			d = dfs(child, i) + 1;
+	int d = 0, max = 0;
+	list<int>::iterator i;
+	for (i = map[child].begin(); i != map[child].end(); i++){
+		if (*i != p) {
+			d = dfs(child, *i) + 1;
 			if (d > max)
 				max = d;
 		}
@@ -82,7 +80,7 @@ int dfs(int p, int child) {
 int findCom() {
 	int i, j, k, count = 0, flag = 0;
 	list<int> l;
-	list<int>::iterator iter;
+	list<int>::iterator iter, jter;
 	int *know = new int[N];
 	for (i = 0; i < N; i++) {
 		know[i] = 0;
@@ -90,18 +88,18 @@ int findCom() {
 	while (1) {
 		flag = 0;
 		for (i = 0; i < N; i++) {
-			if (know[i] == 0) {
+			if (know[i] == 0) {		// find first unknown
 				flag = 1;
 				l.push_back(i);
 				while (!l.empty()) {
 					k = l.front();
 					know[k] = 1;
 					l.pop_front();
-					for (j = 0; j < N; j++)
-						if (know[j] == 0 && m[k][j] == 1) {
+					for (jter = map[k].begin(); jter != map[k].end(); jter++)
+						if (know[*jter] == 0 ) {
 							iter = find(l.begin(), l.end(), j);
 							if(iter == l.end())
-								l.push_back(j);
+								l.push_back(*jter);
 						}
 				}
 				count++;
@@ -112,5 +110,4 @@ int findCom() {
 			break;
 	}
 	return count;
-
 }
