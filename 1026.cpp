44a338172afc82a56c2da432628a86cb1e19e31a
ylkky <867326302@qq.com>
2017-08-29 01:27:13 +0800

    28/30

--- 1026.cpp
+++ 1026.cpp
@@ -11,17 +11,21 @@ using namespace std;
 class P {
 public:
 	int h, m, s, p, vip, t, wt, st;
-	P(int hh, int mm, int ss, int pp, int v) :h(hh), m(mm), s(ss), p(pp), vip(v) {
+	P(int hh, int mm, int ss, int pp, int v) :h(hh), m(mm), s(ss), vip(v) {
 		t = (h - 8) * 60 * 60 + m * 60 + s;
 		wt = -1;
 		st = -1;
+		if (pp > 120)
+			p = 120;
+		else
+			p = pp;
 	};
 	void printST() {
 		int hh, mm, ss, wait;
 		ss = s + wt % 60;
 		mm = m + (wt / 60) % 60;
 		hh = h + wt / (60 * 60);
-		wait = ((wt + 59) / 60) % 60;
+		wait = ((wt + 30) / 60) % 60;
 		printf("%02d:%02d:%02d %d\n", hh, mm, ss, wait);
 		//cout << hh << ":" << mm << ":" << ss << " " << wait << endl;
 	}
@@ -61,6 +65,8 @@ int main() {
 	vector<P> player;
 	for (i = 0; i < N; i++) {
 		cin >> hh >> ch >> mm >> ch >> ss >> p >> vip;
+		if (hh > 21 || hh < 8)
+			continue;
 		P tmp(hh, mm, ss, p, vip);
 		player.push_back(tmp);
 	}
@@ -80,70 +86,50 @@ int main() {
 	}
 	k = 0;
 	kk = 0;		// vip
+	vector<int> vips;
 	P *tmp;
 	for (i = 0; i < CLOSE && k < N; i++) {
 		for (j = 0; j < K; j++) {		// re open a table
 			if (!tbl[j].open) {
 				tmp = &player[tbl[j].id];
 				if ((*tmp).st + (*tmp).p * 60 == i) {
-					tbl[j].open = 1;
+					tbl[j].open = 1;		//reopen
 					tbl[j].id = -1;		// one table is avaliable now
 					//cout << "time = " << i << ", open : " << j << endl;
 				}
 			}
-			if (tbl[j].open) {
-				if (tbl[j].vip) {
-					kk = k;
-					while (player[kk].t <= i) {
-						if (player[kk].vip == 0 || player[kk].st != -1) {	//not vip or has served
-							kk++;
-							continue;
-						}
-						if (player[kk].t <= i && player[kk].vip) {		// a vip is in the queue
-							tbl[j].id = kk;
-							tbl[j].open = 0;
-							tbl[j].cnt++;
-							player[kk].serve(i);
-							//cout << "time = " << i << ", use : " << j << endl;
-							if (kk == k)
-								k++;
-							break;
-						}
-					}
-					if (tbl[j].open) {	//no vip in queue
-						while (player[k].st != -1) k++;		// found 1st player in the queue
-						if (player[k].t <= i) {
-							tbl[j].id = k;
-							tbl[j].open = 0;
-							tbl[j].cnt++;
-							player[k].serve(i);
-							//cout << "time = " << i << ", use : " << j << endl;
-							k++;
-						}
-					}
-				}
-				else		// not a vip table
-				{
-					while (player[k].st != -1) k++;		// found 1st player in the queue
-					if (player[k].t <= i) {
-						tbl[j].id = k;
-						tbl[j].open = 0;
-						tbl[j].cnt++;
-						player[k].serve(i);
-						//cout << "time = " << i << ", use : " << j << endl;
-						k++;
-					}
-				}
-				
+		}
+		kk = k;
+		vips.clear();
+		while (kk < N && player[kk].t <= i) {		// all palyers in the queue
+			if(player[kk].vip && player[kk].st == -1)		//find vip players in  the queue
+				vips.push_back(kk);
+			kk++;
+		}
+		for (j = 0; j < K && !vips.empty(); j++) {	//allocate vip tables first
+			if (tbl[j].open && tbl[j].vip) {
+				kk = vips.front();
+				vips.erase(vips.begin());
+				tbl[j].id = kk;
+				tbl[j].open = 0;
+				tbl[j].cnt++;
+				player[kk].serve(i);
+				player[kk].print();
 			}
 		}
-	}
-	sort(player.begin(), player.end(), serve);
-	for (i = 0; i < N; i++) {
-		if (player[i].st == -1)
-			continue;
-		else {
-			player[i].print();
+		for (j = 0; j < K; j++) {
+			if (tbl[j].open) {
+				while (k < N && player[k].st != -1) k++;		// find 1st player in the queue
+				if (k < N && player[k].t <= i) {
+					tbl[j].id = k;
+					tbl[j].open = 0;
+					tbl[j].cnt++;
+					player[k].serve(i);
+					//cout << "time = " << i << ", use : " << j << endl;
+					player[k].print();
+					k++;
+				}
+			}
 		}
 	}
 	for (i = 0; i < K - 1; i++) {
