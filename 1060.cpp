7b11e75353810d2ab200a0b03854b2f42b530c3b
ylkky <867326302@qq.com>
2017-09-05 20:32:14 +0800

    change -> compare is better than compare -> change

--- 1060.cpp
+++ 1060.cpp
@@ -6,94 +6,50 @@ using namespace std;
 int N;
 char A[MAXD], B[MAXD];
 
-void stanForm(char *s) {
-	int i, j, p, k, length = strlen(s);
-	printf("0.");
-	for (i = 0; i < length; i++)
-		if (s[i] == '.')
-			break;
-	p = i;
-	if (p == 0) {		//0.xxxx
-		for (i = i + 1; i < length; i++)
-			if (s[i] != '0')
-				break;
-		if (i == length) {
-			for (i = 0; i < N; i++)
-				printf("0");
-			printf("*10^0");
-		}
-		else {
-			k = p - i + 1;
-			for (j = i; j < i + N && s[j] != 0; j++)
-				printf("%c", s[j]);
-			if (j < i + N) {
-				for (j; j < i + N; j++)
-					printf("0");
-			}
-			printf("*10^%d", k);
-		}
+int stanForm(char *s) {
+	int i, j, k, n;
+	for (i = 0; s[i] == '0'; i++);
+	if (s[i] == '.') {		//0.000000xxxx
+		for (j = i + 1; s[j] == '0'; j++);
+		n = s[j] == 0? 0 : i - j + 1;
+		for (k = 0; k < N && s[j] != 0; k++, j++)
+			s[k] = s[j];
+		for (k = k; k < N; k++)
+			s[k] = '0';
+		s[k] = 0;
 	}
 	else {
-		k = p;
-		j = 0;
-		for (i = 0; j < N && s[i] != 0; i++) {
-			if (i != p) {
-				printf("%c", s[i]);
-				j++;
-			}
+		for (j = i; s[j] != '.' && s[j] != 0; j++);
+		n = j - i;
+		for (k = 0; i < j && k < N; k++, i++)
+			s[k] = s[i];
+		if (s[i] == '.') {
+			for (i = i + 1; k < N && s[i] != 0; k++, i++)
+				s[k] = s[i];
 		}
-		if (s[i] == 0) {
-			for (i; i < N; i++) {
-				printf("0");
-			}
-		}
-		printf("*10^%d", k);
+		for (k = k; k < N; k++)
+			s[k] = '0';
+		s[k] = 0;
 	}
+	return n;
 }
 
 int main() {
 	//freopen("1", "r", stdin);
-	int i, j, la, lb;
+	int i, a, b;
 	scanf("%d", &N);
 	scanf("%s", A);
 	scanf("%s", B);
-	for (i = 0; A[i] == '0'; i++);
-	strcpy(A, A + i);
-	for (i = 0; B[i] == '0'; i++);
-	strcpy(B, B + i);
-	if (A[0] == '.' && B[0] == '.') {
-		for (la = 1; A[la] == '0'; la++);
-		for (lb = 1; B[lb] == '0'; lb++);
+	a = stanForm(A);
+	b = stanForm(B);
+	if (a != b) {
+		printf("NO 0.%s*10^%d 0.%s*10^%d\n", A, a, B, b);
 	}
 	else {
-		for (la = 0; A[la] != '.' && A[la] != 0; la++);
-		for (lb = 0; B[lb] != '.' && B[lb] != 0; lb++);
-		if (la == lb)
-			la = lb = 0;
-	}
-	if (A[0] == '.' && B[0] == '.' && A[la] != B[lb]) {
-		j = -1;
-	}
-	else if (A[0] != '.' && B[0] != '.' && la != lb)
-		j = -1;
-	else {
-		j = 0;
-		for (i = la; i < N + 1 && j < N; i++, j++) {
-			if (A[i] != B[i])
-				break;
-			if (A[i] == '.')
-				j--;
-		}
-	}
-	if (j < N) {
-		printf("NO ");
-		stanForm(A);
-		printf(" ");
-		stanForm(B);
-	}
-	else {
-		printf("YES ");
-		stanForm(A);
+		if (strcmp(A, B))
+			printf("NO 0.%s*10^%d 0.%s*10^%d\n", A, a, B, b);
+		else
+			printf("YES 0.%s*10^%d\n", A, a);
 	}
 	//fclose(stdin);
 	return 0;
